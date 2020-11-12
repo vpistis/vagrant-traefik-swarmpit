05ecdaee09ae0d838a6f7a564041a9f9d71cf7aa
vpistis <vpistis@users.noreply.github.com>
2020-11-12 23:10:17 +0100

    update traefik configuration

--- post-provision-manager.sh
+++ post-provision-manager.sh
@@ -4,7 +4,8 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Deploy Traefik cluster..."
 git clone https://6d62593bbf2685949cd2de089cd3375f1f3a1d66@github.com/vpistis/vagrant-traefik-swarmpit.git
 docker network create --driver=overlay public
-docker stack deploy -c vagrant-traefik-swarmpit/docker-compose.yml traefik
+cd vagrant-traefik-swarmpit
+docker stack deploy -c docker-compose.yml traefik
 
 echo "Deploy Swarmpit cluster..."
 git clone https://github.com/swarmpit/swarmpit -b master
