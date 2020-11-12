9f80d09a543c6889ed24079c1c34e2c98f9e1502
vpistis <vpistis@users.noreply.github.com>
2020-11-12 19:58:20 +0100

    added docker net creation

--- post-provision-manager.sh
+++ post-provision-manager.sh
@@ -3,6 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "Deploy Traefik cluster..."
 git clone https://6d62593bbf2685949cd2de089cd3375f1f3a1d66@github.com/vpistis/vagrant-traefik-swarmpit.git
+docker network create public
 docker stack deploy -c vagrant-traefik-swarmpit/docker-compose.yml traefik
 
 echo "Deploy Swarmpit cluster..."
